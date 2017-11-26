a985fef2ec58204645bdabacbaeab1bf4edad9c8
Jacqueline Lee <anginblu@gmail.com>
2017-11-26 11:29:13 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,28 @@
+class Discount
+  attr_accessor :discount
+  def initialize(number = 0)
+    @discount = discount
+  end
+end
 
+class CashRegister
+
+attr_accessor :total, :discount
+
+
+  def initialize
+    @total = 0
+    @discount = Discount.new(discount)
+  end
+
+  def cash_register
+    CashRegister.new
+  end
+
+  def cash_register_with_discount
+    discount = Discount.new(20)
+    cash_register = CashRegister.new
+    cash_register.discount = 20
+  end
+
+end
