57697636f99a6a8e7ffc0d193fb1e46ea3a96294
Jacqueline Lee <anginblu@gmail.com>
2017-11-26 11:41:43 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,7 +25,7 @@ attr_accessor :total, :discount
   def apply_discount
     if @discount > 0
       new_total = @total * 0.8
-      @total = new_total_to_inte
+      @total = new_total.to_int
       "After the discount, the total comes to $#{@total}."
     else
       "There is no discount to apply."
